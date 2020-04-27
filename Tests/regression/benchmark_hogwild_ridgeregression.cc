9b4c3998ae8b50c30244ac5549c65b5ac2175c86
abhijit-c <abhijit9331@gmail.com>
2020-04-27 02:50:57 -0400

    Updated gitignore, moved Eigen, initiall code

--- Tests/regression/benchmark_hogwild_ridgeregression.cc
+++ Tests/regression/benchmark_hogwild_ridgeregression.cc
@@ -2,7 +2,8 @@
  * benchmark_hogwild_ridgeregression.cpp
  * author: Abhijit Chowdhary (achowdh2@ncsu.edu)
  *
- * Benchmark HOGWILD! as applied to ridge regression of given dataset:
+ * Benchmark HOGWILD! as applied to ridge regression on a subset of the Million
+ * Song Dataset:
  *
  *  minimize ||Ax-b||_2^2 + \lambda||x||_2^2
  *
@@ -13,6 +14,7 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can 
  * obtain one at http://mozilla.org/MPL/2.0/.
  */
+#include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
 
@@ -22,13 +24,59 @@
 
 #include <Eigen/Dense>
 
-#include "ridgeregression_functions.h"
 #include "readCSV.h"
+
+#define TOL 100.0
+#define LAMBDA 2.0
+#define TRIALS_PER_CORE 10
 
 int 
 main(int argc, char **argv)
 {
-  int P = omp_get_max_threads();
+  unsigned P = omp_get_max_threads();
+  double timings[P];
+
+  // Read MSD dataset into memory and format matrices.
+  Eigen::MatrixXd Data;
+  readCSV<double>("YearPredictionMSD.csv", Data);
+
+  unsigned num_data = Data.rows(); unsigned num_features = Data.cols()-1;
+  unsigned num_train = int(Data.rows()*0.9); unsigned num_test = Data.rows()-num_train;
+
+  Eigen::MatrixXd A = Data.topRightCorner(num_train, num_features);
+  Eigen::MatrixXd T = Data.bottomRightCorner(num_test, num_features);
+  Eigen::VectorXd b = Data.col(0);
+
+  for (unsigned p = 1; p <= P; ++p)
+  { //Perform HOGWILD based ridge regression w/ p cores.
+    omp_set_num_threads(p);
+
+    for (unsigned trial = 0; trial < TRIALS_PER_CORE; ++trial);
+    {
+      srand (1);
+      double t_start, t_end;
+      Eigen::VectorXd x(b.size());
+
+      t_start = omp_get_wtime();
+      
+      unsigned it = 1;
+      while ( (T*x-b).squaredNorm() + LAMBDA*x.squaredNorm() >= TOL )
+      {
+        unsigned i = ran
+        #pragma omp parallel for
+        for (unsigned k = 0; k < p; i++)
+        {
+          unsigned i = rand() % num_train;
+          x = x - 2* //TODO: Complete update step
+        }
+
+      }
+
+      t_end = omp_get_wtime();
+      timings[p] += t_end-t_start;
+    }
+    timings[p] /= TRIALS_PER_CORE;
+  }
 
   return 0;
 }
