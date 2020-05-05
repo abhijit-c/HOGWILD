a6c1e92b354bf015cd4036954d34c404d267fe18
abhijit-c <abhijit9331@gmail.com>
2020-05-05 10:15:03 -0400

    ...

--- Tests/regression/benchmark_hogwild_regression.cc
+++ Tests/regression/benchmark_hogwild_regression.cc
@@ -1,11 +1,11 @@
 /* 
- * benchmark_hogwild_ridgeregression.cpp
+ * benchmark_hogwild_regression.cpp
  * author: Abhijit Chowdhary (achowdh2@ncsu.edu)
  *
- * Benchmark HOGWILD! as applied to ridge regression on a subset of the Million
- * Song Dataset:
+ * Benchmark HOGWILD! as applied to regression on to a simple random normal 50
+ * x 50 matrix A and random normal vector b:
  *
- *  minimize ||Ax-b||_2^2 + \lambda||x||_2^2
+ *  minimize ||Ax-b||_2^2
  *
  * Outputs to results.txt and stdout time taken to reach desired tolerance for
  * each core count possible in system.
@@ -14,21 +14,22 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can 
  * obtain one at http://mozilla.org/MPL/2.0/.
  */
+#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
 
 #include <fstream>
-#include <iostream>
 #include <vector>
 
 #include <Eigen/Dense>
 
 #include "readCSV.h"
 
-#define TOL 100.0
 #define LAMBDA 2.0
-#define NUM_EPOCHS 10
+#define ETA 0.1
+#define TOL 100.0
+#define MAX_ITERATIONS 1000
 #define TRIALS_PER_CORE 10
 
 int 
@@ -39,46 +40,52 @@ main(int argc, char **argv)
 
   // Read MSD dataset into memory and format matrices.
   Eigen::MatrixXd Data;
-  readCSV<double>("YearPredictionMSD.csv", Data);
+  readCSV<double>("simplemat.csv", Data);
 
   unsigned num_data = Data.rows(); unsigned num_features = Data.cols()-1;
-  unsigned num_train = int(Data.rows()*0.9); 
-  unsigned num_test = Data.rows()-num_train;
 
-  Eigen::MatrixXd A = Data.topRightCorner(num_train, num_features);
-  Eigen::MatrixXd T = Data.bottomRightCorner(num_test, num_features);
+  Eigen::MatrixXd A = Data.topRightCorner(num_data, num_features);
   Eigen::VectorXd b = Data.col(0);
+  Eigen::VectorXd x(b.size()); Eigen::MatrixXd X(b.size(), P);
 
   for (unsigned p = 1; p <= P; ++p)
   { //Perform HOGWILD based ridge regression w/ p cores.
     omp_set_num_threads(p);
+    unsigned num_epochs = num_data/p + (num_data % p != 0); //ceil(num_data/p)
 
     for (unsigned trial = 0; trial < TRIALS_PER_CORE; ++trial);
-    {
+    { //Average runtime over number of trials
       srand (1);
       double t_start, t_end;
-      Eigen::VectorXd x(b.size());
-
       t_start = omp_get_wtime();
       
-      unsigned it = 1;
-      while ( (T*x-b).squaredNorm() + LAMBDA*x.squaredNorm() >= TOL )
+      unsigned it = 1; double learning_rate = ETA;
+      while ( 2*(A*x-b).norm() >= TOL && it <= MAX_ITERATIONS )
       {
-        for (unsigned epoch = 0; epoch < NUM_EPOCHS; epoch++)
+        for (unsigned epoch = 0; epoch < num_epochs; epoch++)
         {
           #pragma omp parallel for
-          for (unsigned k = 0; k < p; i++)
+          for (unsigned k = 0; k < p; k++)
           {
-            unsigned i = rand() % num_train;
-            x = x - eta*( 2*(A.row(i)*x - b) + 2*x(i) ); //TODO: implement eta, learning rate
+            unsigned i = rand() % num_data;
+            x = x - learning_rate*( 2*(A.row(i)*x - b) );
           }
+          learning_rate = learning_rate / 2;
         }
       }
 
       t_end = omp_get_wtime();
       timings[p] += t_end-t_start;
     }
+
+    X.col(p-1) = x;
     timings[p] /= TRIALS_PER_CORE;
   }
+
+  for (int p = 0; p < P; p++) { std::cout << timings[p] << ", "; }
+  std::cout << std::endl;
+  const static Eigen::IOFormat CSVFormat(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", "\n");
+  std::ofstream file("results.txt");
+  file << X.format(CSVFormat);
   return 0;
 }
