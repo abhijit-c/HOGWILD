773d46937b4befd135364aecc14269e93a4a2095
abhijit-c <abhijit9331@gmail.com>
2020-05-21 07:29:32 -0400

    Updated heavy_gradient png.

--- Tests/faked_regression/gen_plots.m
+++ Tests/faked_regression/gen_plots.m
@@ -3,7 +3,7 @@ P = (1:length(T))';
 
 figure();
 subplot(1,2,1);
-plot(P, T./T(1), 'k-*');
+plot(P, T(1)./T, 'k-*');
 xlabel('Number of threads');
 ylabel('Speedup relative to serial');
 subplot(1,2,2);
